91d608d07134f99d88afe6655b2a47f21e2dcc19
dainiz <dainiz.almazan@gmail.com>
2022-10-04 10:53:31 -0700

    Adds unit tests for Student class

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,18 +1,34 @@
-class Student:
-    def __init__(self, name, grade, classes):
-        self.name = name
-        self.grade = grade
-        self.classes = classes
+from school_schedule.student import Student
 
-    def add_class(self, class_name):
-        self.classes.append(class_name)
-        return self.classes
+def test_add_class_returns_classes():
+    name = "Bob"
+    grade = "Senior"
+    classes = ["Coding"]
 
-    def get_num_classes(self):
-        return len(self.classes)
+    bob = Student(name, grade, classes)
+    result = bob.add_class("Math")
 
-    def display_classes(self):
-        return(", ".join(self.classes))
+    assert result == ["Coding", "Math"]
+    assert len(result) == 2
+    assert "Math" in result
 
-    def summary(self):
-        return (f"{self.name} is a {self.grade} enrolled in {self.get_num_classes()} classes : {self.display_classes()}")
+def test_empty_list_creates_empty_class_list():
+    name = "Bob"
+    grade = "Senior"
+    classes = []
+
+    bob = Student(name, grade, classes)
+
+    assert len(bob.classes) == 0
+    assert isinstance(bob.classes, list) == True
+
+def test_num_classes_returns_correct_value():
+    name = "Bob"
+    grade = "Senior"
+    classes = ["Coding", "Math", "Science"]
+
+    bob = Student(name, grade, classes)
+    result = bob.get_num_classes()
+
+    assert result == 3
+    assert result == len(bob.classes)
